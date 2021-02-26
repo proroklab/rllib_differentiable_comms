94a00b51c20c5782d6356df0623b55461e086354
J. Blumenkamp <jb2270@cam.ac.uk>
2021-02-26 23:12:45 +0000

    Pass model config as args (as per rllib standard), formatting

--- model.py
+++ model.py
@@ -15,25 +15,29 @@ import copy
 
 torch, nn = try_import_torch()
 
-# https://ray.readthedocs.io/en/latest/using-ray-with-pytorch.html
-
-DEFAULT_OPTIONS = {
-    "encoder_out_features": 8,
-    "shared_nn_out_features_per_agent": 8,
-    "value_state_encoder_cnn_out_features": 16,
-    "share_observations": True,
-}
-
 
 class Model(TorchModelV2, nn.Module):
-    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+    def __init__(
+        self,
+        obs_space,
+        action_space,
+        num_outputs,
+        model_config,
+        name,
+        encoder_out_features,
+        shared_nn_out_features_per_agent,
+        value_state_encoder_cnn_out_features,
+        share_observations,
+    ):
         TorchModelV2.__init__(
             self, obs_space, action_space, num_outputs, model_config, name
         )
         nn.Module.__init__(self)
 
-        self.cfg = copy.deepcopy(DEFAULT_OPTIONS)
-        self.cfg.update(model_config["custom_model_config"])
+        self.encoder_out_features = encoder_out_features
+        self.shared_nn_out_features_per_agent = shared_nn_out_features_per_agent
+        self.value_state_encoder_cnn_out_features = value_state_encoder_cnn_out_features
+        self.share_observations = share_observations
 
         self.n_agents = len(obs_space.original_space["agents"])
         self.outputs_per_agent = int(num_outputs / self.n_agents)
@@ -41,25 +45,26 @@ class Model(TorchModelV2, nn.Module):
         obs_shape = obs_space.original_space["agents"][0].shape
         state_shape = obs_space.original_space["state"].shape
 
+        ###########
+        # Action NN
+
         self.action_encoder = nn.Sequential(
             nn.Linear(obs_shape[0], 32),
             nn.ReLU(),
-            nn.Linear(32, self.cfg["encoder_out_features"]),
+            nn.Linear(32, self.encoder_out_features),
             nn.ReLU(),
         )
 
-        share_n_agents = self.n_agents if self.cfg["share_observations"] else 1
+        share_n_agents = self.n_agents if self.share_observations else 1
         self.action_shared = nn.Sequential(
-            nn.Linear(self.cfg["encoder_out_features"] * share_n_agents, 64),
+            nn.Linear(self.encoder_out_features * share_n_agents, 64),
             nn.ReLU(),
-            nn.Linear(
-                64, self.cfg["shared_nn_out_features_per_agent"] * share_n_agents
-            ),
+            nn.Linear(64, self.shared_nn_out_features_per_agent * share_n_agents),
             nn.ReLU(),
         )
 
         post_logits = [
-            nn.Linear(self.cfg["shared_nn_out_features_per_agent"], 32),
+            nn.Linear(self.shared_nn_out_features_per_agent, 32),
             nn.ReLU(),
             nn.Linear(32, self.outputs_per_agent),
         ]
@@ -67,11 +72,13 @@ class Model(TorchModelV2, nn.Module):
         nn.init.constant_(post_logits[-1].bias, 0)
         self.action_output = nn.Sequential(*post_logits)
 
-        #############
+        ###########
+        # Value NN
+
         self.value_encoder = nn.Sequential(
             nn.Linear(obs_shape[0], 32),
             nn.ReLU(),
-            nn.Linear(32, self.cfg["encoder_out_features"]),
+            nn.Linear(32, self.encoder_out_features),
             nn.ReLU(),
         )
 
@@ -82,23 +89,23 @@ class Model(TorchModelV2, nn.Module):
             SlimConv2d(
                 8, 8, 3, 2, 1
             ),  # in_channels, out_channels, kernel, stride, padding
-            SlimConv2d(8, self.cfg["value_state_encoder_cnn_out_features"], 3, 2, 1),
+            SlimConv2d(8, self.value_state_encoder_cnn_out_features, 3, 2, 1),
             nn.Flatten(1, -1),
         )
 
         self.value_shared = nn.Sequential(
             nn.Linear(
-                self.cfg["encoder_out_features"] * self.n_agents
-                + self.cfg["value_state_encoder_cnn_out_features"],
+                self.encoder_out_features * self.n_agents
+                + self.value_state_encoder_cnn_out_features,
                 64,
             ),
             nn.ReLU(),
-            nn.Linear(64, self.cfg["shared_nn_out_features_per_agent"] * self.n_agents),
+            nn.Linear(64, self.shared_nn_out_features_per_agent * self.n_agents),
             nn.ReLU(),
         )
 
         value_post_logits = [
-            nn.Linear(self.cfg["shared_nn_out_features_per_agent"], 32),
+            nn.Linear(self.shared_nn_out_features_per_agent, 32),
             nn.ReLU(),
             nn.Linear(32, 1),
         ]
@@ -112,10 +119,10 @@ class Model(TorchModelV2, nn.Module):
         device = input_dict["obs"]["state"].device
 
         action_feature_map = torch.zeros(
-            batch_size, self.n_agents, self.cfg["encoder_out_features"]
+            batch_size, self.n_agents, self.encoder_out_features
         ).to(device)
         value_feature_map = torch.zeros(
-            batch_size, self.n_agents, self.cfg["encoder_out_features"]
+            batch_size, self.n_agents, self.encoder_out_features
         ).to(device)
         for i in range(self.n_agents):
             agent_obs = input_dict["obs"]["agents"][i]
@@ -125,19 +132,17 @@ class Model(TorchModelV2, nn.Module):
             input_dict["obs"]["state"].permute(0, 3, 1, 2)
         )
 
-        if self.cfg["share_observations"]:
+        if self.share_observations:
             # We have a big common shared center NN so that all agents have access to the encoded observations of all agents
             action_shared_features = self.action_shared(
                 action_feature_map.view(
-                    batch_size, self.n_agents * self.cfg["encoder_out_features"]
+                    batch_size, self.n_agents * self.encoder_out_features
                 )
-            ).view(
-                batch_size, self.n_agents, self.cfg["shared_nn_out_features_per_agent"]
-            )
+            ).view(batch_size, self.n_agents, self.shared_nn_out_features_per_agent)
         else:
             # Each agent only has access to its own local observation
             action_shared_features = torch.empty(
-                batch_size, self.n_agents, self.cfg["shared_nn_out_features_per_agent"]
+                batch_size, self.n_agents, self.shared_nn_out_features_per_agent
             ).to(device)
             for i in range(self.n_agents):
                 action_shared_features[:, i] = self.action_shared(
@@ -148,13 +153,13 @@ class Model(TorchModelV2, nn.Module):
             torch.cat(
                 [
                     value_feature_map.view(
-                        batch_size, self.n_agents * self.cfg["encoder_out_features"]
+                        batch_size, self.n_agents * self.encoder_out_features
                     ),
                     value_state_features,
                 ],
                 dim=1,
             )
-        ).view(batch_size, self.n_agents, self.cfg["shared_nn_out_features_per_agent"])
+        ).view(batch_size, self.n_agents, self.shared_nn_out_features_per_agent)
 
         outputs = torch.empty(batch_size, self.n_agents, self.outputs_per_agent).to(
             device
